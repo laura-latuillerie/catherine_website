3321c11731bf58e72b203644059e0f978d0de774
Laura Latuillerie <78149250+laura-latuillerie@users.noreply.github.com>
2024-09-11 23:48:13 +0200

    Bouton pillule bleu, sprites pour écran de chargement, bug gallery smartphone et créations de pages

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -1,36 +1,40 @@
-const gallery = document.querySelector(".gallery");
-console.log(gallery.offsetWidth);
+$(window).on('load resize orientationchange', function(){
+	if ($(window).width() < 576) {
+	const gallery = document.querySelector(".gallery");
+	console.log(gallery.offsetWidth);
 
-function getScrollAmount() {
-	let galleryWidth = gallery.scrollWidth + 60;
-	return -(galleryWidth - window.innerWidth);
-}
-
-const tween = gsap.to(gallery, {
-	x: getScrollAmount,
-	duration: 3,
-	ease: "expoScale"
-});
-
-ScrollTrigger.create({
-	trigger: ".gallery",
-	start: "top 0",
-	end: () => `+=${getScrollAmount() * -1}`,
-	pin: true,
-	animation: tween,
-	scrub: 1,
-	invalidateOnRefresh: true,
-	markers: false
-});
-
-ScrollTrigger.observe({
-	target: ".gallery", // can be any element (selector text is fine)
-	type: "pointer,touch", // comma-delimited list of what to listen for ("wheel,touch,scroll,pointer")
-	// onUp: () => previous(),
-	// onDown: () => next(),
-
-	onDrag: (self) => {
-		console.warn(self.deltaX);
-		gsap.to(window, { scrollTo: { y: `+=${self.deltaX * 10}` } });
+	function getScrollAmount() {
+		let galleryWidth = gallery.scrollWidth + 60;
+		return -(galleryWidth - window.innerWidth);
 	}
+
+	const tween = gsap.to(gallery, {
+		x: getScrollAmount,
+		duration: 3,
+		ease: "expoScale"
+	});
+
+	ScrollTrigger.create({
+		trigger: ".gallery",
+		start: "top 0",
+		end: () => `+=${getScrollAmount() * -1}`,
+		pin: true,
+		animation: tween,
+		scrub: 1,
+		invalidateOnRefresh: true,
+		markers: false
+	});
+
+	ScrollTrigger.observe({
+		target: ".gallery", // can be any element (selector text is fine)
+		type: "pointer,touch", // comma-delimited list of what to listen for ("wheel,touch,scroll,pointer")
+		// onUp: () => previous(),
+		// onDown: () => next(),
+
+		onDrag: (self) => {
+			console.warn(self.deltaX);
+			gsap.to(window, { scrollTo: { y: `+=${self.deltaX * 10}` } });
+		}
+	});
+}
 });
