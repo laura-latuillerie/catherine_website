3276a85f13b60ddf0166016c38438e1d0fc75f0e
Laura Latuillerie <78149250+laura-latuillerie@users.noreply.github.com>
2024-10-04 01:14:21 +0200

    modal playlist youtube debug

--- assets/js/youtube-playlist.js
+++ assets/js/youtube-playlist.js
@@ -3,6 +3,7 @@ tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 var player;
+const playlistModal = document.getElementById('playlist');
 
 function onYouTubeIframeAPIReady() {
   player = new YT.Player('player', {
@@ -11,14 +12,30 @@ function onYouTubeIframeAPIReady() {
     videoId: '92UXfEIc4lI',
     events: {
       'onReady': function() {
+
+        playlistModal.addEventListener('hide.bs.modal', () => (stopVideo()));
         $(".video-thumb").click(function() {
           var $this = $(this);
           if (!$this.hasClass("active")) {
             player.loadVideoById($this.attr("data-video"));
-                        $this.addClass("active").siblings().removeClass("active");
+            $this.addClass("active").siblings().removeClass("active");
           }
         });
-      }
+      },
     }
   });
 }
+
+// 4. The API will call this function when the video player is ready.
+function onPlayerReady(event) {
+  event.target.playVideo();
+  player.mute();
+}
+
+function stopVideo() {
+  player.stopVideo();
+}
+
+
+
+
