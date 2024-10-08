db850f069def0cf90e899438228ed83928c972c1
Laura Latuillerie <78149250+laura-latuillerie@users.noreply.github.com>
2024-10-08 22:35:09 +0200

    Past Events Opé

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -66,45 +66,9 @@ $('#carousel-formules-photos').slick({
   ]
  });
 
- $('#carousel-projets').slick({
-  dots: false,
-  infinite: true,
-  speed: 300,
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  responsive: [
-   {
-     breakpoint: 1200,
-     settings: {
-       slidesToShow: 3,
-       slidesToScroll: 3
-     }
-   },
-     {
-       breakpoint: 992,
-       settings: {
-         slidesToShow: 2,
-         slidesToScroll: 2
-       }
-     },
-    {
-      breakpoint: 768,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
-  ]
- });
-
 
   /* Slick needs no get Reinitialized on window Resize after it was destroyed */
   $(window).on('load resize orientationchange', function() {
-    
-  
         var $carousel = $('#carousel-team-polaroid');
         /* Initializes a slick carousel only on mobile screens */
         // slick on mobile
