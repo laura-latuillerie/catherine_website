6762cb405854a90cd5e763d1b36da76a2fbbf93d
Laura Latuillerie <78149250+laura-latuillerie@users.noreply.github.com>
2024-10-09 15:38:16 +0200

    Début de télé

--- assets/js/youtube.js
+++ assets/js/youtube.js
@@ -1,9 +1,17 @@
+const btnUnmute = document.querySelector('#btnUnmute');
+const btnMute = document.querySelector('#btnMute');
+const btnTurn = document.querySelector('#btnTurn');
+const btnNext = document.querySelector('#btnNext');
+const btnPrev = document.querySelector('#btnPrev');
+const lists = ['PL4pvheXYumia1FQOPEqFbc4MZptoCy79e', 'PL4pvheXYumiZ21sq6Sw-dsXxF6m5eK5Ef', 'PL4pvheXYumiaDiJ8QfNffCLX77ojov8sf', 'PL4pvheXYumiYojZV4b_ea10y6TNxObELh', 'PL4pvheXYumiZd6rdS-eyRInVw-81sGC7d']
+
 // 2. This code loads the IFrame Player API code asynchronously.
 var tag = document.createElement('script');
 
 tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
 
 // 3. This function creates an <iframe> (and YouTube player)
 //    after the API code downloads.
@@ -15,9 +23,13 @@ function onYouTubeIframeAPIReady() {
     playerVars: 
           { 'autoplay': 1,
            'controls': 0,
+           'rel': 0,
+           'ecver': 2,
+           'showinfo': 0,
+           'disablekb': 1,
            enablejsapi: 1,
             listType:'playlist',
-            list: 'PL4pvheXYumia1FQOPEqFbc4MZptoCy79e'
+            list: 'PL4pvheXYumia1FQOPEqFbc4MZptoCy79e',
           },
     events: {
       'onReady': onPlayerReady,
@@ -29,6 +41,15 @@ function onYouTubeIframeAPIReady() {
 function onPlayerReady(event) {
   event.target.playVideo();
   player.mute();
+  btnUnmute.addEventListener('click', () => (player.unMute()));
+  btnMute.addEventListener('click', () => (player.mute()));
+  btnTurn.addEventListener('click', function () {
+    if(player.getPlayerState()==1){ //Returns true if video is playing
+      player.pauseVideo();
+    }else{
+      player.playVideo(); 
+    }
+  });
 }
 
 function stopVideo() {
