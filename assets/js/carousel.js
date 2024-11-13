1cc4ecd37eafd7c0d1698b4a5448450efb87c562
Laura Latuillerie <78149250+laura-latuillerie@users.noreply.github.com>
2024-11-13 14:14:22 +0100

    Portfolio

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -36,8 +36,8 @@ $('#carousel-formules-photos').slick({
   dots: true,
   infinite: true,
   speed: 300,
-  slidesToShow: 4,
-  slidesToScroll: 4,
+  slidesToShow: 3,
+  slidesToScroll: 3,
   responsive: [
    {
      breakpoint: 1200,
